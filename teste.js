ec1ae5b1e75559921165afbf7699c92524d7f80d
Roberto Sena <robertoprogsena@gmail.com>
2024-07-26 15:53:01 -0300

    Teste inicial

--- teste.js
+++ teste.js
@@ -1,14 +1,5 @@
-let listaDeLinguagens = [];
-
 function aperteBotao() {    
-
-    let palavra = prompt("Digite uma linguagem")
-    listaDeLinguagens.push(palavra);
-    let posicaoDaLista = parseInt(prompt("Digite qual posição da lista você quer acessar"));
-    posicaoDaLista--;
-    console.log(`O item ${posicaoDaLista + 1} da lista é: ${listaDeLinguagens[posicaoDaLista]}`)
-    console.log(listaDeLinguagens.length)
-    let ultimoElemento = listaDeLinguagens[listaDeLinguagens.length-1];
-    console.log(ultimoElemento)
+    var teste = prompt("Teste de codigo");
+    
 }
 
